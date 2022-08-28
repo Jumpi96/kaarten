d6a51c64556e0d16d7a941c4b26eb00ca75bbe9b
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2022-08-28 21:06:57 +0200

    Testing tf versions

--- deployment/main.tf
+++ deployment/main.tf
@@ -2,17 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.62.0"
+      version = "~>3.27"
     }
   }
 
+  required_version = ">=0.14.9"
+
   backend "s3" {
-    bucket = "botsyn-terraform-state-bucket"
+    bucket = "kaarten-terraform-state-bucket"
     key    = "state/terraform_state.tfstate"
     region = "us-west-2"
   }
 }
 
 provider "aws" {
+  version = "~>3.0"
   region = "us-west-2"
 }
