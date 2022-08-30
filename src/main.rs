5a026b9b8be8e7f147b2de2b98c80541a8625476
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2022-08-31 00:09:13 +0200

    Fixing message processing

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ async fn main() -> Result<(), Error> {
 async fn func(event: ApiGatewayProxyRequest, _: Context) -> Result<ApiGatewayProxyResponse, Error> {
     let body = &event.body;
 
+    log::info!("Body received: {}", body.as_ref().unwrap_or(&String::from("no body")));
+
     let empty_res = ApiGatewayProxyResponse {
         status_code: 201,
         headers: HeaderMap::new(),
@@ -31,16 +33,22 @@ async fn func(event: ApiGatewayProxyRequest, _: Context) -> Result<ApiGatewayPro
 
     let body: serde_json::Value =
         serde_json::from_str(body.as_ref().unwrap_or(&String::from("{}"))).unwrap();
+    
+    log::info!("Message body received: {}", body);
 
     match body.get("message") {
+        Some(message) => match message.as_object() {
+            Some(message_obj) => match message_obj.get("text") {
+                Some(text) => match text.as_str() {
+                    Some(text) if text.starts_with("/add") => handlers::add_handler(&body).await,
+                    Some(_) => log::info!("Discarding unknown input..."),
+                    None => log::error!("Discarding invalid input...")
+                },
+                None => log::error!("Discarding invalid input...")
+            },
+            None => log::error!("Discarding invalid input...")
+        },
         None => log::error!("Message doesn't exist!"),
-        msg => {
-            let command = msg.unwrap().as_str().unwrap();
-            match command {
-                command if command.starts_with("/add") => handlers::add_handler(&body).await,
-                _ => log::info!("Discarding unknown input...")
-            }
-        }
     }
     Ok(empty_res)
 }
